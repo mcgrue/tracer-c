a823ffcf1ebfa372ff12e3e2ae785683ce219638
Ben McGraw <ben.mcgraw@gmail.com>
2020-05-13 18:08:40 -0700

    Created a way to make a dict of unique words to store tokens in

--- tracer-c/tracer-c.c
+++ tracer-c/tracer-c.c
@@ -1,29 +1,41 @@
 // tracer-c.cpp : Defines the entry point for the console application.
 //
 
+#include "assert.h"
 #include "stdio.h"
 #include "cJSON.h"
 #include "cJSON_Utils.h"
 
 #include "uthash.h" // http://troydhanson.github.io/uthash/
 
-char** global_token_table = NULL; // put all unique strings here.  Then reference them by pointer in the tracery_particle.
+typedef struct {
+	const char* word;
+	UT_hash_handle hh;
+} unique_word;
 
-typedef struct tracery_particle {
+unique_word* unique_wordset = NULL;
+
+/*
+typedef struct {
 	char* word;     // "Taco"
 	char* reroll;   // "#foo#"
 	char* modifier; // ".capitalize" // probably a finite set of them.  Can they be chained?
 	char* named;    // "[foo:#bar#]"
 
+	
+} tracery_particle;
+
+typedef struct {
+	tracery_particle* key;
+	tracery_particle** line;
 	UT_hash_handle hh;
-};
+} trace;
 
-// doesnt compile if uncommented
-// typedef struct trace {
-//	tracery_particle* line;
-//};
+typedef struct {
 
-// typedef grammar will be a hash of rerolls to => trace*'s
+	UT_hash_handle hh;
+} grammar;
+*/
 
 char* load_file(char* filename) {
 	char* buffer = 0;
@@ -46,8 +58,35 @@ char* load_file(char* filename) {
 	return buffer;
 }
 
-int main()
-{
+/// adds a new word to unique_wordset if it's not already there.
+void add_unique_word(char* word) {
+	
+	unique_word* uw;
+	
+	HASH_FIND_STR(unique_wordset, word, uw);  // id already in the hash?
+	
+	if( uw == NULL ) {
+		uw = malloc(sizeof(unique_word));
+		uw->word = word;
+		HASH_ADD_KEYPTR(hh, unique_wordset, uw->word, strlen(uw->word), uw);
+	}
+}
+
+char* print_all_unique_words() {
+	unique_word* uw;
+
+	char buffer[255] = "";
+
+	for (uw = unique_wordset; uw != NULL; uw = uw->hh.next) {
+		printf("%s,", uw->word);
+		strcat(buffer, uw->word);
+		strcat(buffer, ",");
+	}
+
+	return strdup(buffer);
+}
+
+void do_json_stuff() {
 	char* raw_json_string = 0;
 
 	printf("Hello, world\n\n");
@@ -77,7 +116,7 @@ int main()
 	printf("before dump\n\n");
 
 	// const cJSON *obj = cJSON_GetObjectItemCaseSensitive(parsed_json, "resolutions");;
-	printf( cJSON_Print(obj) );
+	printf(cJSON_Print(obj));
 
 	printf("after dump\n\n");
 
@@ -85,4 +124,55 @@ end:
 	printf("ending...");
 
 	return 0;
+}
+
+int test_dictionary_uniqueness() {
+	add_unique_word("taco");
+	add_unique_word("burrito");
+	add_unique_word("muffin");
+	add_unique_word("jones");
+	add_unique_word("burrito");
+	add_unique_word("muffin");
+	add_unique_word("alice");
+	add_unique_word("taco");
+
+	char* result = print_all_unique_words();
+	char* expected = "taco,burrito,muffin,jones,alice,";
+
+	int ret = 0;
+	if ( strcmp(result, expected) == 0 ) {
+		printf("test_dictionary_uniqueness PASSED!\n");
+	}
+	else {
+		printf("test_dictionary_uniqueness FAILED\n!\n '%s' != '%s'", result, expected);
+		ret = 1;
+	}
+
+	free(result); 
+	result = NULL;
+	return ret;
+}
+
+void run_all_tests() {
+	int fails = 0;
+
+	fails += test_dictionary_uniqueness();
+
+	assert(fails == 0);
+}
+
+int main()
+{
+	run_all_tests();
+
+	// do_json_stuff()
+
+
+	unsigned int num_words;
+	num_words = HASH_COUNT(unique_wordset);
+	printf("there are %u words\n", num_words);
+
+	print_all_unique_words();
+
+		printf("huah\n");
 }
